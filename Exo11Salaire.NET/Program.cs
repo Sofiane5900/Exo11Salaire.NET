81a94a86edefb683b820ca1c7a82e91ad3df59ee
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2024-12-24 11:36:39 +0100

    premiere partie fini, début creation IHM

--- Exo11Salaire.NET/Program.cs
+++ Exo11Salaire.NET/Program.cs
@@ -1,20 +1,26 @@
-﻿using Exo11Salaire.NET;
+﻿using System.ComponentModel.Design;
+using Exo11Salaire.NET;
 
 Salarié chloé = new Salarié(18232, "Cantine", "Chloé", 24000 );
-Salarié emma = new Salarié(18232, "Bureau", "Emma", 26000);
-Salarié goerges = new Salarié(18232, "Informatique", "Goerges", 28000);
+Salarié emma = new Salarié(25424, "Bureau", "Emma", 26000);
+Salarié goerges = new Salarié(1182, "Informatique", "Goerges", 28000);
 
 List<Salarié> listeSalariés = new List<Salarié>();
 listeSalariés.Add(chloé);
 listeSalariés.Add(emma);
 listeSalariés.Add(goerges);
 
-foreach (Salarié s in listeSalariés)
+while (true)
 {
-    Console.WriteLine(s);
+    Console.WriteLine("---Menu Salariées--");
 }
 
- void AfficherSalaire()
+foreach (Salarié s in listeSalariés)
 {
-    Console.WriteLine();
+    AfficherSalaire(s);
+}
+
+ void AfficherSalaire(Salarié salarié)
+{
+    Console.WriteLine($"Le salaire de {salarié.Nom} (matricule: {salarié.Matricule}) est de {salarié.Salaire} euros.");
 }
