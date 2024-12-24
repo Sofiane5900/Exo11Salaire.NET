b1ea91babe7225fb86c90e2a6d83773a05c1a782
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2024-12-24 11:32:30 +0100

    premiere progression

--- Exo11Salaire.NET/Program.cs
+++ Exo11Salaire.NET/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exo11Salaire.NET;
+
+Salarié chloé = new Salarié(18232, "Cantine", "Chloé", 24000 );
+Salarié emma = new Salarié(18232, "Bureau", "Emma", 26000);
+Salarié goerges = new Salarié(18232, "Informatique", "Goerges", 28000);
+
+List<Salarié> listeSalariés = new List<Salarié>();
+listeSalariés.Add(chloé);
+listeSalariés.Add(emma);
+listeSalariés.Add(goerges);
+
+foreach (Salarié s in listeSalariés)
+{
+    Console.WriteLine(s);
+}
+
+ void AfficherSalaire()
+{
+    Console.WriteLine();
+}
---
+++ Exo11Salaire.NET/Salarié.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo11Salaire.NET
+{
+    internal class Salarié
+    {
+        private int _matricule;
+        private string _service;
+        private string _nom;
+        private int _salaire;
+
+        public int Matricule { get => _matricule; set => _matricule = value; }
+        public string Service { get => _service; set => _service = value; }
+        public string Nom { get => _nom; set => _nom = value; }
+        public int Salaire { get => _salaire; set => _salaire = value; }
+
+
+        public Salarié(int Matricule, string Service, string Nom, int Salaire)
+        {
+            _matricule = Matricule;
+            _service = Service;
+            _nom = Nom;
+            _salaire= Salaire;
+        }
+    }
+}
